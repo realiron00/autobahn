055b200a1c60c017b0f9f51016be3150b35070fb
realiron00 <ic1595@kookmin.ac.kr>
2023-11-02 15:53:51 +0900

    11021553

--- autobahn_div.c
+++ autobahn_div.c
@@ -15,7 +15,49 @@ Output: (Q, R) such that A = BQ + R (0 ≤ R < B).
 8: end for
 9: return (Q, R)
 */
+/*
+x=yq+r(0<=r<y)
+*/
+
+/*
+A, B ∈ Z≥0
+Output: INVALID or (Q, R) such that A = BQ + R, (0 ≤ R < B).
+1: procedure DIVnaive(A, B)
+2: if B ≤ 0 then
+3: return INVALID
+4: end if
+5: if A < B then
+6: return (0, A)
+7: end if
+8: if B = 1 then
+9: return (A, 0)
+10: end if
+11: (Q, R) ← (0, A)
+12: while R ≥ B do
+13: (Q, R) ← (Q + 1, R − B)
+14: end while
+15: return (Q, R)
+16: end procedure
+
+*/
 void bi_long_div(bi** q, bi** r, bi* x, bi* y)
 {
+        //if y<=0, return INVALID
+        if (bi_is_zero(y) && y->sign == 1){
+                exit(1);
+        }
 
+        //if x<y, return q=0, r=x
+        if (bi_cmp(x,y)==-1){
+                bi_set_zero(q);
+                bi_cpy(r,x);
+                return;
+        }
+
+        //if y=1, return q=x, r=0
+        if(bi_is_one(y)){
+                bi_cpy(q,x);
+                bi_set_zero(r);
+                return;
+        }
 }
