b40794443fe0ffb26e343eda5fd02e262eebdd47
realiron00 <ic1595@kookmin.ac.kr>
2023-11-23 15:02:57 +0900

    2311231502

--- autobahn_div.c
+++ autobahn_div.c
@@ -176,7 +176,47 @@ void bi_div_naive(bi** q, bi** r, bi* x, bi* y)
     bi_delete(&bi_one);
 }
 
+/*
+Multi-Precision Long Division
+A = A_0 + A_1*W^1 + ... + A_n-1*W^n-1, B (A_j ∈ [0, W))
+Output: (Q = Q_0 + Q_1*W^1 + ... + Q_n-1*W^n-1, R)
+such that A = BQ + R (0 ≤ R < B, Q_j ∈ [0, W)).
+1: procedure DIVlong(A, B)
+2: if A < B then
+3: return (0, A)
+4: end if
+5: R_n ← 0
+6: for i = n − 1 downto 0 do
+7: (Q_i, R_i) ← DIVC(R_i+1*W + A_i, B). 
+8: end for
+9: Q ← Q_0 + Q_1*W + ... + Q_n−1*W^n−1
+10: return (Q, R0)
+11: end procedure
+*/
+/*
+2-word long division
+Input: A = A_1W + A_0, B
+(A_1, A_0 ∈ [0, W), B ∈ [2^w−1, 2^w), A_1 < B, A0 = a_0 + a_1*2^1 + ... + a_w-1*2^w-1)
+Output: Q such that A = BQ + R (0 ≤ R < B, Q_j ∈ [0, W)).
+1: (Q, R) ← (0, A_1)
+2: for j = w − 1 downto 0 do
+3:  if R ≥ 2^w−1 then
+4:      (Q, R) ← (Q + 2^j, 2R + a_j − B). 2R + a_j − B = R + a_j − (B − R) < W
+5:  else
+6:      R ← 2R + a_j. R ∈ [0, W)
+7:      if R ≥ B then
+8:          (Q, R) ← (Q + 2^j, R − B)
+9:      end if
+10: end if
+11: end for
+12: return Q
+*/
+
+//bi_div_general_long: Multi-Precision Long Division
+//DIVC : 2-word long division
 void bi_div_general_long(bi** q, bi** r, bi* x, bi* y)
 {
+    bi_div_discriminant(q, r, x, y);
 
+    
 }
